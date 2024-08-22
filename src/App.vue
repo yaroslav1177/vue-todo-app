2e4aa257e650d4d33f7a7be5204a9b5755377210
Avramidi Yaroslav <passenger1177@gmail.com>
2024-08-22 13:23:41 +0300

    add result

--- src/App.vue
+++ src/App.vue
@@ -1,132 +1,124 @@
+<script>
+import StatusFilter from "./components/StatusFilter.vue";
+import TodoItem from "./components/TodoItem.vue";
+
+export default {
+  components: {
+    StatusFilter,
+    TodoItem,
+  },
+
+  data() {
+    let todos = [];
+    const jsonData = localStorage.getItem("todos") || "[]";
+
+    try {
+      todos = JSON.parse(jsonData);
+    } catch (e) {}
+
+    return {
+      todos,
+      title: "",
+      status: "all",
+    };
+  },
+  computed: {
+    activeTodos() {
+      return this.todos.filter((todo) => !todo.completed);
+    },
+    completedTodos() {
+      return this.todos.filter((todo) => todo.completed);
+    },
+    visibleTodos() {
+      switch (this.status) {
+        case "active":
+          return this.activeTodos;
+
+        case "completed":
+          return this.completedTodos;
+
+        default:
+          return this.todos;
+      }
+    },
+    allCompleted() {
+      return this.todos.length > 0 && this.todos.every(todo => todo.completed);
+    }
+  },
+  watch: {
+    todos: {
+      deep: true,
+      handler() {
+        localStorage.setItem("todos", JSON.stringify(this.todos));
+      },
+    },
+  },
+  methods: {
+    handleSubmit() {
+      if (this.title.trim() === "") return;
+      this.todos.push({
+        id: Date.now(),
+        title: this.title,
+        completed: false,
+      });
+      this.title = "";
+    },
+    toggleAll() {
+      const allCompleted = this.allCompleted;
+      this.todos.forEach(todo => todo.completed = !allCompleted);
+    },
+    clearCompleted() {
+      this.todos = this.todos.filter(todo => !todo.completed);
+    }
+  },
+};
+</script>
+
 <template>
+  <video autoplay muted loop>
+    <source src="./background/snow.mp4" type="video/mp4" />
+  </video>
   <div class="todoapp">
-    <h1 class="todoapp__title">todos</h1>
+    <h1 class="todoapp__title">Todos</h1>
 
     <div class="todoapp__content">
       <header class="todoapp__header">
-        <button type="button" class="todoapp__toggle-all active" />
-        <form>
+        <button
+          type="button"
+          class="todoapp__toggle-all"
+          :class="{ active: activeTodos.length === 0 }"
+          @click="toggleAll"
+        ></button>
+        <form @submit.prevent="handleSubmit">
           <input
-            data-cy="NewTodoField"
             type="text"
             class="todoapp__new-todo"
             placeholder="What needs to be done?"
+            v-model="title"
           />
         </form>
       </header>
 
-      <section class="todoapp__main" data-cy="TodoList">
-        <div data-cy="Todo" class="todo completed">
-          <label class="todo__status-label">
-            <input
-              data-cy="TodoStatus"
-              type="checkbox"
-              class="todo__status"
-              checked
-            />
-          </label>
+      <TransitionGroup name="list" tag="section" class="todoapp__main">
+        <TodoItem
+          v-for="(todo, index) of visibleTodos"
+          :key="todo.id"
+          :todo="todo"
+          @update="Object.assign(todo, $event)"
+          @delete="todos.splice(todos.indexOf(todo), 1)"
+        />
+      </TransitionGroup>
 
-          <span data-cy="TodoTitle" class="todo__title">
-            Completed Todo
-          </span>
+      <footer class="todoapp__footer">
+        <span class="todo-count"> {{ activeTodos.length }} items left </span>
 
-          <button type="button" class="todo__remove" data-cy="TodoDelete">
-            ×
-          </button>
-        </div>
-
-        <div data-cy="Todo" class="todo">
-          <label class="todo__status-label">
-            <input
-              data-cy="TodoStatus"
-              type="checkbox"
-              class="todo__status"
-            />
-          </label>
-
-          <span data-cy="TodoTitle" class="todo__title">
-            Not Completed Todo
-          </span>
-
-          <button type="button" class="todo__remove" data-cy="TodoDelete">
-            ×
-          </button>
-        </div>
-
-        <div data-cy="Todo" class="todo">
-          <label class="todo__status-label">
-            <input
-              data-cy="TodoStatus"
-              type="checkbox"
-              class="todo__status"
-            />
-          </label>
-
-          <form>
-            <input
-              data-cy="TodoTitleField"
-              type="text"
-              class="todo__title-field"
-              placeholder="Empty todo will be deleted"
-              value="Todo is being edited now"
-            />
-          </form>
-        </div>
-
-        <div data-cy="Todo" class="todo">
-          <label class="todo__status-label">
-            <input
-              data-cy="TodoStatus"
-              type="checkbox"
-              class="todo__status"
-            />
-          </label>
-
-          <span data-cy="TodoTitle" class="todo__title">
-            Todo is being saved now
-          </span>
-
-          <button type="button" class="todo__remove" data-cy="TodoDelete">
-            ×
-          </button>
-        </div>
-      </section>
-
-      <footer class="todoapp__footer" data-cy="Footer">
-        <span class="todo-count" data-cy="TodosCounter">
-          3 items left
-        </span>
-
-        <nav class="filter" data-cy="Filter">
-          <a
-            href="#/"
-            class="filter__link selected"
-            data-cy="FilterLinkAll"
-          >
-            All
-          </a>
-
-          <a
-            href="#/active"
-            class="filter__link"
-            data-cy="FilterLinkActive"
-          >
-            Active
-          </a>
-
-          <a
-            href="#/completed"
-            class="filter__link"
-            data-cy="FilterLinkCompleted"
-          >
-            Completed
-          </a>
-        </nav>
+        <StatusFilter v-model="status" />
 
         <button
           type="button"
           class="todoapp__clear-completed"
-          data-cy="ClearCompletedButton"
+          v-if="completedTodos.length > 0"
+          @click="clearCompleted"
         >
           Clear completed
         </button>
@@ -134,3 +126,33 @@
     </div>
   </div>
 </template>
+
+<style>
+.list-enter-active,
+.list-leave-active {
+  max-height: 60px;
+  transition: all 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  max-height: 0;
+  transform: scaleY(0);
+}
+body {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  height: 100vh;
+}
+video {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 100vw !important;
+  height: 100vh !important;
+  object-fit: cover;
+  transform: translate(-50%, -50%);
+  z-index: -1;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './assets/styles/index.scss';
-
-import { createApp } from 'vue'
-import App from './App.vue'
 
 createApp(App).mount('#app')
